2f8666a4ac4edbf5b2458f852e34687ad7a551ac
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-02 19:15:17 +0800

    added countdown script

--- ex01/ft_countdown.ml
+++ ex01/ft_countdown.ml
@@ -1,2 +1,20 @@
-print_int
-print_char
+let rec ft_countdown n = 
+  if n < 0 then 
+    begin 
+      print_int 0;
+      print_char '\n'
+    end
+  else
+    begin
+      print_int n;
+      print_char '\n';
+      if n > 0 then ft_countdown (n - 1)
+    end
+
+let () = 
+  print_string "Test with 3:\n";
+  ft_countdown 3;
+  print_string "Test with 0:\n";
+  ft_countdown 0;
+  print_string "Test with -1:\n";
+  ft_countdown (-1)
