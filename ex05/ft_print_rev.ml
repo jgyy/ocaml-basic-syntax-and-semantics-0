42699ac8edc30014a29b85e96cfde61cf9f43d32
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-02 19:53:20 +0800

    added print reverse script

--- ex05/ft_print_rev.ml
+++ ex05/ft_print_rev.ml
@@ -1,3 +1,16 @@
-print_char
-String.get
-String.length
+let ft_print_rev str =
+  let len = String.length str in
+  let rec print_reverse_helper i =
+    if i >= 0 then begin
+      print_char (String.get str i);
+      print_reverse_helper (i - 1)
+    end
+  in
+  print_reverse_helper (len - 1);
+  print_char '\n'
+
+let () =
+  ft_print_rev "Hello world !";
+  ft_print_rev "";
+  ft_print_rev "42";
+  ft_print_rev "ocaml"
