df8fe260474e793b7a343105abb61435ffe14490
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-02 19:10:43 +0800

    added test sign script

--- ex00/ft_test_sign.ml
+++ ex00/ft_test_sign.ml
@@ -1,1 +1,10 @@
-print_endline
+let ft_test_sign x = 
+  if x >= 0 then
+    print_endline "positive"
+  else
+    print_endline "negative"
+
+let () =
+  ft_test_sign 42;
+  ft_test_sign 0;
+  ft_test_sign (-42)
