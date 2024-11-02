9bb8dcf56b43e35a51c52907f70e85aa70dc763c
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-02 19:45:50 +0800

    print all alphabet

--- ex03/ft_print_alphabet.ml
+++ ex03/ft_print_alphabet.ml
@@ -1,3 +1,10 @@
-char_of_int
-int_of_char
-print_char
+let ft_print_alphabet () =
+  let rec print_char_range current last =
+    print_char current;
+    if current < last then
+      print_char_range (char_of_int (int_of_char current + 1)) last
+  in
+  print_char_range 'a' 'z';
+  print_char '\n'
+
+let () = ft_print_alphabet ()
