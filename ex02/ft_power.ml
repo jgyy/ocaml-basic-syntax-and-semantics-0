23c841e946a52238d36cfd2721606c0485938a31
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-02 19:25:31 +0800

    added power script

--- ex02/ft_power.ml
+++ ex02/ft_power.ml
@@ -1,1 +1,14 @@
-ft_power.ml
+let rec ft_power x n =
+  if n = 0 then 1
+  else if x = 0 then 0 
+  else x * ft_power x (n - 1)
+
+let () =
+  let test x n =
+    Printf.printf "ft_power %d %d = %d\n" x n (ft_power x n)
+  in
+  test 2 4;
+  test 3 0;
+  test 0 5;
+  test 5 2;
+  test 3 3
