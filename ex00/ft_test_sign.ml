145f4e15204e66d1e15ce5c3d8dc00bbf022fe2f
Jeffrey Goh <jegoh@c1r1s3.42singapore.sg>
2024-11-03 16:16:16 +0800

    testing ocamlopt in vm

--- ex00/ft_test_sign.ml
+++ ex00/ft_test_sign.ml
@@ -1,12 +1,13 @@
-let ft_test_sign x = 
+let ft_test_sign x =
   if x >= 0 then
     print_endline "positive"
   else
     print_endline "negative"
 
-(*
+(* ocamlopt ft_test_sign.ml; ./a.out *)
+
 let () =
   ft_test_sign 42;
   ft_test_sign 0;
   ft_test_sign (-42)
-*)
+
